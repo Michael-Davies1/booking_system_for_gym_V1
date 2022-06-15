6063501807f8df95c094afe1e2b99ec19fe08b7b
MD210515 <michaeldavies924@gmail.com>
2022-06-15 13:26:52 +0100

    added action for sign in sign up and staff sign in buttons

--- src/main/java/com/example/booking_system_for_gym_v1/HelloController.java
+++ src/main/java/com/example/booking_system_for_gym_v1/HelloController.java
@@ -4,16 +4,29 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.Scanner;
+
 public class HelloController {
+    Scanner input = new Scanner(System.in);
     @FXML
-    public void pressButton (ActionEvent event ) {
-        System.out.println("hello");
+    public void staffSignInButton (ActionEvent event) {
+        System.out.println("Enter staff E-mail");
+        String staff_email_input = input.next();
+        System.out.println("Enter password");
+        String staff_password_input;
     }
     @FXML
-    private Label welcomeText;
-
+    public void signInButton (ActionEvent event) {
+        System.out.println("Enter E-mail");
+        String user_email_input = input.next();
+        System.out.println("Enter password:");
+        String user_password_input = input.next();
+    }
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void signUpButton (ActionEvent event) {
+        System.out.println("Enter E-mail");
+        String create_user_email = input.next();
+        System.out.println("Enter password");
+        String create_user_password = input.next();
     }
 }
